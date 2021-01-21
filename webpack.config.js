d15dd2d85adde23586a5df081e7e0a3daea47991
Fabrice <rafalimananafabrice@gmail.com>
2021-01-21 09:23:22 +0300

    style css only

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,7 @@ Encore
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
     .addEntry('app', './assets/app.js')
-    // .addEntry('style', './assets/styles/app.css')
+    .addStyleEntry('style', './assets/styles/style.css')
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
     // .enableStimulusBridge('./assets/controllers.json')
@@ -70,7 +70,7 @@ Encore
     //.enableIntegrityHashes(Encore.isProduction())
 
     // uncomment if you're having problems with a jQuery plugin
-    //.autoProvidejQuery()
+    .autoProvidejQuery()
 ;
 
 module.exports = Encore.getWebpackConfig();
